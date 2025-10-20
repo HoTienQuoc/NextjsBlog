247d76e03cd1f978261c5da8d2f7fde8bec5d6c6
HoTienQuoc <hotienquoc0429@gmail.com>
2025-10-20 12:52:49 +0700

    Update package dependencies and global styles; added 'class-variance-authority', 'react-icons', and 'tw-animate-css' packages, and enhanced global styles with new CSS variables for improved theming and layout consistency.

--- nextjsblog/components/layout/NavBar.tsx
+++ nextjsblog/components/layout/NavBar.tsx
@@ -1,5 +1,7 @@
 import Container from "./Container";
 import ThemeToggle from "./ThemeToggle";
+import { MdNoteAlt } from "react-icons/md";
+
 
 const Navbar = () => {
     return (
@@ -7,8 +9,8 @@ const Navbar = () => {
             <Container>
                 <div className="flex justify-between items-center gap-8">
                     <div className="flex items-center gap-1 cursor-pointer">
-                        <div>Icon</div>
-                        <div>WEBDEV.BLOG</div>
+                        <MdNoteAlt size={24}/>
+                        <div className="font-bold text-xl">WEBDEV.BLOG</div>
                     </div>
                     <div>Search</div>
                     <div className="flex gap-5 sm:gap-8 items-center">
